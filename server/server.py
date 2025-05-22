4394750c589f1a481f0197da7a66bea84c74905b
Mendiadi <98290644+Mendiadi@users.noreply.github.com>
2025-05-22 12:44:19 +0300

    Update server.py

--- server/server.py
+++ server/server.py
@@ -3,11 +3,18 @@ import os
 import json
 import uuid
 from datetime import datetime
-from pymongo import MongoClient
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+uri = "mongodb+srv://itsshokobo:btNjtK4xgwnGSeNf@cluster0.gsohlmo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
 
-# MongoDB Connection
-MONGO_URI = "mongodb+srv://itsshokobo:btNjtK4xgwnGSeNf@cluster0.gsohlmo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-client = MongoClient(MONGO_URI)
 db = client['flask_codes_db']
 redemption_collection = db['redemption_codes']
 used_collection = db['used_codes']
